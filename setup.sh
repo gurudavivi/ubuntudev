6de467d5d0a79c9cfda993c687d1d85c0af2fb72
vivianeflowt <vivianeflowt@gmail.com>
2022-05-07 01:46:58 -0300

    step

--- setup.sh
+++ setup.sh
@@ -20,7 +20,6 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 # echo "export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm" >> ~/.zshrc
 
-bash
 
 nvm install node
 nvm install-latest-npm
